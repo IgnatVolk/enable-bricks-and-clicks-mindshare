7f9949d46d79594885a38a83421659b9a45694f0
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-12-02 23:29:14 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,62 @@
+
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+}
+    }
+    const data = [];
+    });
+    data.forEach(item => {
+main();
+    for (let i = 0; i < 10; i++) {
+
+function main() {
+    return data;
+}
+
+function generateRandomData() {
+def generate_random_data():
+import random
+
+
+    main()
+    data = generate_random_data()
+def main():
+
+    return data
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+        print(f"Random Number: {item}")    });
+    data.forEach(item => {
+main();
+        console.log("Random Number: " + item);
+
+
+
+    const data = [];
+    const data = generateRandomData();
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    }
+}
+    return data;
+}
+function generateRandomData() {
+}
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    }
+    for (let i = 0; i < 10; i++) {
+function main() {
+
+
+    data.forEach(item => {
+function generateRandomData() {
+    const data = generateRandomData();
+}
+main();
+        console.log("Random Number: " + item);
+    const data = [];
