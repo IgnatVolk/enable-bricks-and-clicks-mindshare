eef26d969ceccc01fc79f51e5565ab815c2c3528
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-12-03 01:00:21 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+        console.log("Random Number: " + item);
+    return data;
+    const data = generateRandomData();
+}
+}
+
+    for (let i = 0; i < 10; i++) {
+    }
+    });
+
+
+main();
+function main() {
+    data.forEach(item => {
+function generateRandomData() {
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
